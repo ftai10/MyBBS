af9cc2591a01942705989310d4a990577e767cc7
Taito Fujii <taito.fujii@avanade.com>
2020-07-03 22:12:43 +0900

    Table定義追加

---
+++ MyBBS/BBSDatabase/Thread.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Thread]
+(
+	[ThreadId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [isOrigin] BIT NOT NULL, 
+    [UserId] INT NOT NULL, 
+    [CreateData] DATETIME NOT NULL, 
+    [UpdateData] DATETIME NOT NULL, 
+    [OriginThreadId] INT NULL, 
+    [Title] VARCHAR(128) NOT NULL,
+    [PostText] VARCHAR(MAX) NOT NULL
+)
---
+++ MyBBS/BBSDatabase/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [LoginId] NVARCHAR(16) NOT NULL, 
+    [Password] VARBINARY(16) NOT NULL, 
+    [CreateData] DATETIME NOT NULL, 
+    [UpdateData] DATETIME NULL
+)
